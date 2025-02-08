e637cee9d2e5d46919f885793b1a90977bc04b36
Younesss37 <younesbelkhaddar4@gmail.com>
2025-02-08 14:38:17 +0100

    Create KindeConfig.jsx

---
+++ utils/KindeConfig.jsx
@@ -0,0 +1,8 @@
+import { KindeSDK } from "@kinde-oss/react-native-sdk-0-7x";
+
+export const client = new KindeSDK(
+  "https://lamiini.kinde.com",
+  "exp://192.168.11.121:8081",
+  "2a84c7f9c62640fca54701e47252ad9c",
+  "exp://192.168.11.121:8081"
+);
